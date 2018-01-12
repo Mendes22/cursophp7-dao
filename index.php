979c9c9e5fabd5c65a9d5a99f9adb80069e638b1
Maycon Mendes <mayconmendes01@gmail.com>
2018-01-12 17:08:01 -0200

    Aperfeiçoei a classe usuarios

--- index.php
+++ index.php
@@ -2,15 +2,26 @@
 
 require_once("config.php");
 
+$acesso = new Usuario();
+
+$acesso->login("Sarah", "Martins");
+
+echo $acesso;
+
+/*
+$search = usuario::search("j");
+echo json_encode($search);
+
+
+$lista = usuario::getList(); 
+echo json_encode($lista);
+
+
 $user = new Usuario();
-
-
 $user->loadById(7);
-
 echo $user;
 
-
-/*$sql = new Sql();
+$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios ");
  
